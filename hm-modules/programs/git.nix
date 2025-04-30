92991b7db9a39f60077fdac81545cae1ab5c4a01
Alex Peterson <for.alexpeterson@gmail.com>
2025-04-29 23:59:44 -0700

    some git configs

--- hm-modules/programs/git.nix
+++ hm-modules/programs/git.nix
@@ -2,12 +2,32 @@
 {
     programs.git = {
         enable = true;
+
         userName = "Alex Peterson";
         userEmail = "for.alexpeterson@gmail.com";
 
+        ignores = [
+            "*.swp"
+            ".DS_Store"
+        ];
+
+        diff-so-fancy = {
+            enable = true;
+            markEmptyLines = false;
+            stripLeadingSymbols = false;
+        };
+
         extraConfig = {
+            core.whitespace = "error";
             pull.rebase = true;
             init.defaultBranch = "main";
+
+            status = {
+                branch = true;
+                short = true;
+                showStash = true;
+                showUntrackedFiles = "all";
+            };
         };
     };
 }
