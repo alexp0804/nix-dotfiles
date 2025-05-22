e8f40f34d109cc279d40f271870a926a83453ca5
Alex Peterson <for.alexpeterson@gmail.com>
2025-05-21 19:59:04 -0700

    tmux improvements

--- hm-modules/programs/tmux.nix
+++ hm-modules/programs/tmux.nix
@@ -5,17 +5,25 @@
         enable = true;
         baseIndex = 1;
         mouse = true;
-        prefix = "C-a";
+        prefix = "C-Space";
         terminal = "screen-256color";
         keyMode = "vi";
         historyLimit = 10000;
-        plugins = [
-            pkgs.tmuxPlugins.sensible
-            pkgs.tmuxPlugins.vim-tmux-navigator
-            pkgs.tmuxPlugins.yank
-            pkgs.tmuxPlugins.tokyo-night-tmux
-            pkgs.tmuxPlugins.pain-control
+
+        plugins = with pkgs.tmuxPlugins; [
+            sensible
+            vim-tmux-navigator # use ctrl+hjkl to navigate between nvim and tmux panes
+            yank # clipboard integration
+            pain-control # for better pane splitting/resizing shortcuts
+            {
+                plugin = tokyo-night-tmux;
+                extraConfig = ''
+                    # disable datetime widget
+                    set -g @tokyo-night-tmux_show_datetime 0
+                '';
+            }
         ];
+
         extraConfig = ''
             # renumber windows when another window is closed
             set -g renumber-windows on
